23466600afb37dfefdb12a925fa2ff3770835491
mellobot <mellobot@ccc35a16-0c5e-4362-a522-c3ee09183623>
2015-07-14 19:46:53 +0000

    added HtmlSparrow project. It aims to make websites with HTML and FTP. I used Filezilla but you cannot use a WYSIWYG system with filezilla..

---
+++ trunk/simdesign/HtmlSparrow/HtmlSparrow.dpr
@@ -0,0 +1,13 @@
+program HtmlSparrow;
+
+uses
+  Forms,
+  HtmlSparrowMain in 'HtmlSparrowMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
